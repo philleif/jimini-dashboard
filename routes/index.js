e34eb754f6cf24f67952737d796314df608756cc
Philip Bjerknes <philleif@Philips-iMac-Pro.local>
2018-02-14 18:53:06 -0500

    vue placeholder for position list

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,32 @@ const db = require("../lib/db")
 const router = express.Router()
 
 /* GET home page. */
+router.get("/positions", async (req, res, next) => {
+  let positions = await db.Position.find().select({
+    pair: 1,
+    amount: 1,
+    timeframe: 1,
+    time: 1,
+    exchange: 1,
+    openPrice: 1,
+    closePrice: 1,
+    status: 1,
+    _id: 0
+  })
+
+  const closedPositions = positions.filter(
+    position => position.status === "CLOSED"
+  )
+  const openPositions = positions.filter(position => position.status === "OPEN")
+
+  res.json({
+    positions: {
+      open: openPositions,
+      closed: closedPositions
+    }
+  })
+})
+
 router.get("/", async (req, res, next) => {
   let closedPositions = await db.Position.find({ status: "CLOSED" }).select({
     pair: 1,
