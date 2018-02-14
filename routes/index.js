4ab99f5f224cbfd964c6ffbf8885d298d1cab602
Philip Bjerknes <philleif@Philips-iMac-Pro.local>
2018-02-14 17:45:22 -0500

    populate dashboard with data

--- routes/index.js
+++ routes/index.js
@@ -6,18 +6,39 @@ const router = express.Router()
 
 /* GET home page. */
 router.get("/", async (req, res, next) => {
-  let allPositions = await db.Position.find({ status: "CLOSED" }).select({
+  let closedPositions = await db.Position.find({ status: "CLOSED" }).select({
+    pair: 1,
+    amount: 1,
+    timeframe: 1,
+    time: 1,
+    exchange: 1,
     openPrice: 1,
     closePrice: 1,
     _id: 0
   })
 
+  let openPositions = await db.Position.find({ status: "OPEN" }).select({
+    pair: 1,
+    amount: 1,
+    timeframe: 1,
+    time: 1,
+    exchange: 1,
+    openPrice: 1,
+    _id: 0
+  })
+
+  let value = 0
+  for (let position of closedPositions) {
+    value += position.amount * (position.closePrice - position.openPrice)
+  }
+
   res.render("index", {
     positions: {
-      today: allPositions,
-      total: allPositions,
-      open: allPositions,
-      closed: allPositions
+      today: openPositions,
+      total: openPositions,
+      open: openPositions,
+      closed: closedPositions,
+      value: Math.round(value)
     }
   })
 })
