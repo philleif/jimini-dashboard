36f6be6a684afa5d1fd9a1562952b30be12a9380
Philip Bjerknes <philleif@Philips-iMac-Pro.local>
2018-02-15 07:03:11 -0500

    add agendash

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
+require("dotenv").config()
+
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+const Agenda = require('agenda')
+const Agendash = require('agendash')
 
 var index = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
+
+var agenda = new Agenda({db: {address: process.env.DB_URL}})
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -24,7 +30,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
-
+app.use('/agenda', Agendash(agenda))
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@
 
 const express = require("express")
 const db = require("../lib/db")
+
 const router = express.Router()
 
 /* GET home page. */
