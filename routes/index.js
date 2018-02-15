322ea8a13502ff6abdcdc9e4f50ee20ca978ff52
Philip Bjerknes <philleif@Philips-iMac-Pro.local>
2018-02-14 19:52:20 -0500

    add closed positions

--- routes/index.js
+++ routes/index.js
@@ -18,10 +18,12 @@ router.get("/positions", async (req, res, next) => {
     _id: 0
   })
 
-  const closedPositions = positions.filter(
+  let openPositions = positions.filter(position => position.status === "OPEN")
+  let closedPositions = positions.filter(
     position => position.status === "CLOSED"
   )
-  const openPositions = positions.filter(position => position.status === "OPEN")
+
+  console.log(closedPositions)
 
   res.json({
     positions: {
@@ -32,41 +34,7 @@ router.get("/positions", async (req, res, next) => {
 })
 
 router.get("/", async (req, res, next) => {
-  let closedPositions = await db.Position.find({ status: "CLOSED" }).select({
-    pair: 1,
-    amount: 1,
-    timeframe: 1,
-    time: 1,
-    exchange: 1,
-    openPrice: 1,
-    closePrice: 1,
-    _id: 0
-  })
-
-  let openPositions = await db.Position.find({ status: "OPEN" }).select({
-    pair: 1,
-    amount: 1,
-    timeframe: 1,
-    time: 1,
-    exchange: 1,
-    openPrice: 1,
-    _id: 0
-  })
-
-  let value = 0
-  for (let position of closedPositions) {
-    value += position.amount * (position.closePrice - position.openPrice)
-  }
-
-  res.render("index", {
-    positions: {
-      today: openPositions,
-      total: openPositions,
-      open: openPositions,
-      closed: closedPositions,
-      value: Math.round(value)
-    }
-  })
+  res.render("index")
 })
 
 module.exports = router
